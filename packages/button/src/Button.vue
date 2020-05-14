c13dcb13827bca3a1715d725fb7effc292b7ce70
WengJiankai <0049003261@znv.com>
2020-05-14 23:02:43 +0800

    将button组件样式抽离

--- packages/button/src/Button.vue
+++ packages/button/src/Button.vue
@@ -4,7 +4,7 @@
     class="yl-button"
     :class="[`yl-button--${type}`, { 'is-plain': plain, 'is-round': round }]"
   >
-    <span>
+    <span v-if="$slot.default">
       <slot></slot>
     </span>
   </button>
@@ -33,60 +33,3 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-.yl-button {
-  display: inline-block;
-  margin: 0;
-  padding: 12px 20px;
-  background: #fff;
-  border: 1px solid #dcdfe6;
-  border-radius: 4px;
-  // color: #1e4377;
-  color: #606266;
-  font-size: 14px;
-  font-weight: 500;
-  text-align: center;
-  box-sizing: border-box;
-  outline: none;
-  transition: 0.1s;
-  // 禁止元素文字被选中
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  line-height: 1;
-  white-space: nowrap;
-  cursor: pointer;
-  &:hover,
-  &:focus {
-    color: #409eff;
-    border-color: #c6e2ff;
-    background: #ecf5ff;
-  }
-}
-.yl-button--primary {
-  background-color: #409eff;
-  color: #fff;
-  border-color: #409eff;
-  &:hover,
-  &:focus {
-    background-color: #66b1ff;
-    color: #fff;
-    border-color: #66b1ff;
-  }
-}
-.yl-button--primary.is-plain {
-  background-color: #ecf5ff;
-  color: #a2a8ad;
-  border-color: #b3d8ff;
-  &:hover,
-  &:focus {
-    background-color: #66b1ff;
-    color: #fff;
-    border-color: #66b1ff;
-  }
-}
-.yl-button--primary.is-round {
-  padding: 12px 23px;
-  border-radius: 20px;
-}
-</style>
